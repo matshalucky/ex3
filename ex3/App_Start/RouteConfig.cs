8c06e42eda77d4144711c208b3db3dbe9c20d842
matshalucky <matsha.matan@gmail.com>
2019-05-28 16:41:24 +0300

    url action

--- ex3/App_Start/RouteConfig.cs
+++ ex3/App_Start/RouteConfig.cs
@@ -12,12 +12,24 @@ namespace ex3
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: "Display",
+                url: "Display/{ip}/{port}",
+                defaults: new { controller = "First", action = "Map" }
+            );
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: "DisplayRoute",
+                url: "Display/{ip}/{port}/{time}",
+                defaults: new { controller = "First", action = "displayRoute" }
+            );
 
             routes.MapRoute(
-                name: "Default",
-                url: "Display/{ip}/{port}",
-                defaults: new { controller = "First", action = "Map"}
-            );
+              name: "Default",
+              url: "{action}/{id}",
+              defaults: new { controller = "First", action = "Index" , id = UrlParameter.Optional}
+          );
         }
     }
 }
--- ex3/Controllers/FirstController.cs
+++ ex3/Controllers/FirstController.cs
@@ -2,13 +2,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using System.Windows.Input;
+using System.Diagnostics;
 //
 namespace ex3.Controllers
 {
     public class FirstController : Controller
     {
+        public string Test()
+        {
+            return "success";
+        }
+
+        [HttpPost]
+        public string ToXml()
+        {
+            Random rnd = new Random();
+            string lon = Commands.Instance.getData("get /position/longitude-deg") + rnd.Next(50);
+            string lat = Commands.Instance.getData("get /position/latitude-deg") + rnd.Next(50);
+            //Initiate XML stuff
+            StringBuilder sb = new StringBuilder();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            XmlWriter writer = XmlWriter.Create(sb, settings);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Location");
+            writer.WriteElementString("lon", lon);
+            writer.WriteElementString("lat", lat);
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
+            return sb.ToString();
+
+        }
         // GET: First
         public ActionResult Index()
         {
@@ -26,6 +55,14 @@ namespace ex3.Controllers
             return View();
         }
 
+        public ActionResult displayRoute(string ip, int port , int time)
+        {
+            Commands.Instance.connect(ip, port);
+            Session["time"] = time;
 
+
+
+            return View();
+        }
     }
 }
